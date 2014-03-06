1c32e7769e4165468eba6d3eddeedcad325dfd6b
Jake Richan <jake.richan@moneydesktop.com>
2014-03-06 10:50:14 -0700

    pluckAttributeWithStringFormat should check if the item is a model. If it is, return the models attribute value

--- src/Graft.js
+++ src/Graft.js
@@ -94,6 +94,8 @@
       var parsed = Tools.parseAttributeFromString(string);
       if (parsed.isFunction && !dontCallFunctions) {
         return item[parsed.name]();
+      } else if (item instanceof Backbone.Model) {
+        return item.get(string);
       } else {
         return item[parsed.name];
       }
